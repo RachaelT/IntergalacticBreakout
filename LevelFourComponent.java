d07aef18d5b58ebbe4cd2fa364f45e17943e1b5d
Rachael Thompson <Rachaelst@mac.com>
2017-05-19 16:18:04 -0500

    updated lv 4

--- LevelFourComponent.java
+++ LevelFourComponent.java
@@ -19,7 +19,12 @@ import java.awt.geom.Ellipse2D;
 
 public class LevelFourComponent extends LevelComponent
 {
-
+	private static final int WIDTH = 50;
+	private static final int HEIGHT = 30;
+	private static final int BLUE = 5;
+	private static final int INDIGO = 6;
+	private static final int VIOLET = 7;
+	private static final int MAGENTA = 8;
 
 	/**
 	 * 
@@ -36,112 +41,142 @@ public class LevelFourComponent extends LevelComponent
     public void init()
     {
     	//Generates Brick pattern
-		//Magenta swirl
+    	//Generates Brick pattern
+    	//Magenta swirl
     	ArrayList<Brick> bricks = super.getBricks();
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 350, 0, 8));
-		}	
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 300, 30, 8));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 200, 60, 8));
-		}
-		for(int i = 0; i < 4; i++)
-		{
-			bricks.add(new Brick((i * 50) + 100, 90, 8));
-		}
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 200, 120, 8));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 250, 150, 8));
-		}
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 300, 180, 8));
-		}
-    	
-    	//Violet Swirl
     	for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 350, 390, 7));
-		}	
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 400, 360, 7));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 450, 330, 7));
-		}
-		for(int i = 0; i < 4; i++)
-		{
-			bricks.add(new Brick((i * 50) + 500, 300, 7));
-		}
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 500, 270, 7));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 400, 240, 7));
-		}
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 350, 210, 7));
-		}
-		
-		//Indigo Swirl
-		bricks.add(new Brick(400, 180, 6));
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 400, 150, 6));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 450, 120, 6));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 550, 90, 6));
-		}
-		bricks.add(new Brick(600, 60, 6));
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 650, 120, 6));
-		}
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50) + 700, 150, 6));
-		}
-		bricks.add(new Brick(750, 180, 6));
-		
-		//Blue Swirl
-		bricks.add(new Brick(300, 210, 5));
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 200, 240, 5));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 150, 270, 5));
-		}
-		for(int i = 0; i < 3; i++)
-		{
-			bricks.add(new Brick((i * 50) + 50, 300, 5));
-		}
-		bricks.add(new Brick(100, 330, 5));
-		for(int i = 0; i < 2; i++)
-		{
-			bricks.add(new Brick((i * 50), 270, 5));
-		}
-		bricks.add(new Brick(0, 240, 5));
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * VIOLET),
+    						              0, MAGENTA));
+    			}	
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * INDIGO),
+    									  HEIGHT, MAGENTA));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (BLUE - 1)),
+    									  HEIGHT * 2, MAGENTA));
+    			}
+    			for(int i = 0; i < (2 +2); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * 2), HEIGHT *
+    									 (BLUE - 2), MAGENTA));
+    			}
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (BLUE - 1)),
+    						    		  HEIGHT * (BLUE - 1), MAGENTA));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * BLUE),
+    								      HEIGHT * BLUE, MAGENTA));
+    			}
+    			
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * INDIGO),
+    								      HEIGHT * INDIGO, MAGENTA));
+    			}
+    	    	
+    	    	//Violet Swirl
+    	    	for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * VIOLET),
+    									  HEIGHT * (MAGENTA + BLUE), VIOLET));
+    			}	
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * MAGENTA), 
+    								     HEIGHT * (MAGENTA + BLUE - 1), VIOLET));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + 1)),
+    									  HEIGHT * (MAGENTA + 2 + 1), VIOLET));
+    			}
+    			for(int i = 0; i < (2 + 2); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + 2)),
+    								      HEIGHT * (MAGENTA + 2), VIOLET));
+    			}
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + 2)),
+    								      HEIGHT * (MAGENTA + 1), VIOLET));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * MAGENTA),
+    									  HEIGHT * MAGENTA, VIOLET));
+    			}
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * VIOLET), 
+    									  HEIGHT * VIOLET, VIOLET));
+    			}
+    			
+    			//Indigo Swirl
+    			bricks.add(new Brick(WIDTH * MAGENTA, HEIGHT * INDIGO, 
+    								INDIGO));
+    			
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * MAGENTA ), 
+    									  HEIGHT * BLUE, INDIGO));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + 1)),
+    									  HEIGHT * (BLUE - 1), INDIGO));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + 2 + 1)),
+    									  HEIGHT * (2 + 1), INDIGO));
+    			}
+    			bricks.add(new Brick(WIDTH * (MAGENTA + 2 + 2), HEIGHT * 2, INDIGO));
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + BLUE)), 
+    							     	  HEIGHT * (BLUE - 1), INDIGO));
+    			}
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (MAGENTA + INDIGO)), 
+    									  HEIGHT * BLUE, INDIGO));
+    			}
+    			bricks.add(new Brick(WIDTH * (MAGENTA + 2 + BLUE), HEIGHT * INDIGO, 
+    								 INDIGO));
+    			
+    			//Blue Swirl
+    			bricks.add(new Brick(WIDTH * INDIGO, HEIGHT * VIOLET, BLUE));
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (BLUE - 1)), 
+    									  HEIGHT * MAGENTA, BLUE));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + (WIDTH * (2 + 1)), 
+    								      HEIGHT * (MAGENTA + 1), BLUE));
+    			}
+    			for(int i = 0; i < (2 + 1); i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH) + WIDTH, HEIGHT * 
+    							  		 (MAGENTA + 2), BLUE));
+    			}
+    			bricks.add(new Brick(WIDTH * 2, HEIGHT * (MAGENTA + 2 + 1),
+    						         BLUE));
+    			
+    			for(int i = 0; i < 2; i++)
+    			{
+    				bricks.add(new Brick((i * WIDTH), HEIGHT * (MAGENTA + 1),
+    									 BLUE));
+    			}
+    			bricks.add(new Brick(0, HEIGHT * MAGENTA, BLUE));
 		
     	//Adds a ball
 		ArrayList<Ball> balls = super.getBalls(); 
